1eb72449373ce2fd8578098fbed9d2bd1ae19064
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-10 23:37:44 +0530

    weather app task HackerKernel first commit

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+// 	let params = new URLSearchParams({
+// 		q: 'London',
+// 		limit: '2',
+// 		appid: 'fe7525cb58077d1151f33f61b2576dc5',
+// 	});
+
+// 	fetch(
+// 		`https://api.openweathermap.org/data/2.5/weather?lat=23.2584857&lon=77.401989&appid=38563a45e910840c283837a6959d2880
+// `
+// 	)
+// 		.then(res => res.json())
+// 		.then(console.log);
+	return (
+	<>
+  
+  </>
+	);
+}
