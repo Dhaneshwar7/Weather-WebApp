e56abf0d1440683110f2fa452cd88697f986849c
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-13 18:34:38 +0530

    update v7

--- src/components/WeatherComponents/WeatherBox.jsx
+++ src/components/WeatherComponents/WeatherBox.jsx
@@ -97,11 +97,11 @@ const WeatherBox = () => {
 				<div className="WeatherInfo drop-shadow-box dark:drop-shadow-dark  bg-l-col dark:bg-d-col/50 w-3/5 max-sm:min-h-[50vh] h-full p-3 rounded-2xl max-sm:w-full max-sm:gap-4 max-sm:py-6 max-sm:px-5 flex max-sm:flex-col">
 					<div className="Left flex flex-col items-center container max-sm:flex-row justify-center m-auto ">
 						<div className="Temperature container text-center m-auto  py-3 mb-3 max-sm:mb-0">
-							<h1 className="text-6xl max-sm:text-6xl">
-								{wdata.temperature} &deg;C
+							<h1 className="text-5xl max-sm:text-[44px]">
+								{wdata.temperature}&deg;C
 							</h1>
 							<div className="FeelLike max-sm:mt-2">
-								<h3> Feels Like : {wdata.feelsLike}&deg;C</h3>{' '}
+								<h3 className='max-sm:text-[15px]'> Feels Like : {wdata.feelsLike}&deg;C</h3>{' '}
 							</div>
 						</div>
 						<div className="SunRiseSunSet container flex items-center justify-center flex-col gap-2 m-auto">
