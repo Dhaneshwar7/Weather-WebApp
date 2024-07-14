9480e0f553444679877f32071dc701fe2df816ee
Delta Mike <87529812+DELTAMIKE7@users.noreply.github.com>
2024-07-14 10:21:17 +0530

    error boundary removed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,14 @@
 import '@/styles/globals.css';
-import ErrorBoundary from '@/utils/ErrorBoundary';
 import { WeatherDataProvider } from '@/utils/WeatherDataReducer';
 import { ThemeProvider } from 'next-themes';
 
 export default function App({ Component, pageProps }) {
 	return (
-		<ErrorBoundary>
 			<ThemeProvider attribute="class">
 				<WeatherDataProvider>
 					<Component {...pageProps} />
 				</WeatherDataProvider>
 			</ThemeProvider>
-		</ErrorBoundary>
+
 	);
 }
